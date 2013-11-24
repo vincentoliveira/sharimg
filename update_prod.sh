194da70dcb23dbd89be452f601a1b98552ae3d54
Vincent Oliveira <vincent@oliveira.fr>
2013-11-24 15:29:54 +0100

    update_prod

--- update_prod.sh
+++ update_prod.sh
@@ -6,18 +6,9 @@ php composer.phar update
 chmod -R 0777 app/cache
 chmod -R 0777 app/logs
 
-sudo -u www-data php app/console doctrine:database:drop --force --env=prod
-sudo -u www-data php app/console doctrine:database:create --env=prod
 sudo -u www-data php app/console doctrine:schema:update --force --env=prod
-
-sudo -u www-data php app/console fos:user:create admin admin@sharimg.com admin123 --super-admin --env=prod
-
 sudo -u www-data php app/console assets:install web --symlink
 sudo -u www-data php app/console fos:js-routing:dump
-
-echo "Clear images"
-rm -rf web/images/*
-mkdir web/images/min
 
 sudo -u www-data php app/console cache:clear --env=prod --no-warmup
 sudo -u www-data php app/console cache:warmup --env=prod --no-debug
